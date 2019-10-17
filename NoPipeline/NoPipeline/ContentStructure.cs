f12b44481f3f32061d925f275a3fd7c7cb00202b
gn Fur <patych7@gmail.com>
2019-10-18 01:01:17 +0300

    Added ContentStructure.

---
+++ NoPipeline/NoPipeline/ContentStructure.cs
@@ -0,0 +1,15 @@
+﻿
+namespace NoPipeline
+{
+	public static class ContentStructure
+	{
+		public const string GlobalPropertiesDivider = "#----------------------------- Global Properties ----------------------------#";
+		public const string ReferencesDivider = "#-------------------------------- References --------------------------------#";
+		public const string ContentDivider = "#---------------------------------- Content ---------------------------------#";
+
+		public const string ReferenceKeyword = "/reference:";
+
+		public const string ContentBeginKeyword = "#begin";
+
+	}
+}
